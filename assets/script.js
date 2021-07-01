917b784c9ccf7f5c26eec694b3fd1db818961147
rangamboa <rangamboa@gmail.com>
2021-06-30 23:50:02 -0700

    coding seemingly successful for current time color change for block

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,55 @@
-var globalActiveTime;
+// Delcare global variables.
+var activeTime;
+var dateNow;
+var timeNow;
+var timeBlock;
+var taskEl;
+var saveEl;
 
-function displayActiveTime() {
-    var dateNow;
-    globalActiveTime = setInterval(function() {
-        dateNow = moment().format("dddd [-] D MMMM YYYY")
-        // console.log(timeNow);
+// Check current date and time at regular intervals.
+function checkDateTime() {
+
+    activeTime = setInterval(function() {
+        dateNow = moment().format("dddd [-] D MMMM YYYY");
+        timeNow = moment().format("HH");
+        //timeNow = 'task'+timeNow;
+        console.log(timeNow);
         $("#currentDate").text(dateNow);
+
+        timeNow = 11;
+
+        // Check if current time is within business hours. If so, change background color of current timeblock to red.
+        if (timeNow < 9 || timeNow > 17) {
+            console.log('not business hours');
+        } else {
+            console.log('business time!');
+            timeNow = 'task' + timeNow;
+            
+            taskEl = $('.cellTask');
+            taskEl.each(function() {
+
+                timeBlock = $(this).attr('id');
+
+                if (timeNow == timeBlock) {
+                    $(this).css("background-color", "tomato");
+                }
+            })
+
+        }
+
     }, 1000);
 }
 
-displayActiveTime();
+function smth() {
 
-var hi = $('#task9a');
+    var hi = $('#task9a');
 
-hi.text('Love Challenges, Be Intrigued by Mistakes');
+    hi.text('Love Challenges, Be Intrigued by Mistakes');
 
-var cellTaskEl = $('.cellTask');
+    var cellTaskEl = $('.cellTask');
 
-console.log(cellTaskEl);
-cellTaskEl.append(' appended text');
+    console.log(cellTaskEl);
+    cellTaskEl.append(' appended text');
+}
+
+checkDateTime();
