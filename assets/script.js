9e93c66c04427433216369e37378f0c56cd6a697
rangamboa <rangamboa@gmail.com>
2021-07-01 15:35:48 -0700

    further testing and optimizing of code for color blocking

--- assets/script.js
+++ assets/script.js
@@ -9,45 +9,40 @@ var saveEl;
 // Check current date and time at regular intervals.
 function checkDateTime() {
 
+    // Check date/time every second.
     activeTime = setInterval(function() {
 
-        // Pull date from moment.js.
-        dateNow = moment().format("dddd [-] D MMMM YYYY");
+        // Pull current date from moment.js.
+        dateNow = moment().format("dddd[,] DD MMMM YYYY");
         $("#currentDate").text(dateNow);
 
-        // Pull time from moment.js.
+        // Pull current hour from moment.js.
         timeNow = moment().format("H");
-        console.log(timeNow);
 
-        // Check if current time is within business hours. If so, change background color of current timeblock to red.
-        if (timeNow < 9 || timeNow > 17) {
-            console.log('not business hours');
-        } else {
-            console.log('business time!');
+        // Check if current time is within business hours (in military time).
 
-            // Pulls all elements containing a timeblock.
-            taskEl = $('.cellTask');
+        // Pulls all elements containing a timeblock.
+        taskEl = $('.cellTask');
 
-            // Iterate through each "task" td.
-            taskEl.each(function() {
+        // Iterate through each "task" td.
+        taskEl.each(function() {
 
-                // Pull number characters from ID to compare to current time.
-                timeBlock = $(this).attr('id');
-                timeBlock = timeBlock.substr(timeBlock.length - 2);
-                if (timeBlock === 'k9') timeBlock = 9;
-                console.log(timeBlock);
+            // Pull number characters from ID to compare to current time.
+            timeBlock = $(this).attr('id');
+            timeBlock = timeBlock.substr(timeBlock.length - 2);
+            if (timeBlock === 'k9') timeBlock = 9;
 
-                // Change background color of each timeblock depending on current time.
+            // Change background color of each timeblock depending on current time:
 
-                if (timeNow > timeBlock) $(this).css("background-color", "grey");
-                else if (timeNow == timeBlock) $(this).css("background-color", "tomato");
-                else $(this).css("background-color", "green");
-            })
+            // Change to grey if before/after business hours or if before current time.
+            if (timeNow > timeBlock || timeNow < 9 || timeNow > 17) $(this).css("background-color", "lightgrey");
 
-        }
+            // Change to red if equal to current time. 
+            else if (timeNow == timeBlock) $(this).css("background-color", "tomato");
+
+            // Otherwise, color will remain green for the rest of the workday.
+        })
     }, 1000);
 }
-
-saveEl = $('.cellSave');
 
 checkDateTime();
